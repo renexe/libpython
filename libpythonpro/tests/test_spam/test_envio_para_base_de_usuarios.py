613d8e4f3d4517fcb244b92320cb8c41bafb3afd
Rene <rene.exe@gmail.com>
2020-08-09 18:44:43 -0300

    Isolando teste enviador_de_email através de injeção de dependências

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -20,11 +20,39 @@ from libpythonpro.tests.test_spam.conftest import sessao
 def test_qde_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = EnviadorMock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'rene@email.com',
         'Educação EAD',
         'Aprendendo ACAD em dois meses'
     )
-    assert len(usuarios) == enviador.qtd_emails_enviados
+    assert len(usuarios) == enviador.qtd_email_enviados
+
+
+class EnviadorMock(Enviador):
+    def __init__(self):
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Rene', email='rene@email.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'pedro@email.com',
+        'Educação EAD',
+        'Aprendendo ACAD em dois meses'
+    )
+    assert enviador.parametros_de_envio == (
+        'pedro@email.com',
+        'rene@email.com',
+        'Educação EAD',
+        'Aprendendo ACAD em dois meses'
+    )
