3696910b0a479f1cdd4a76f76ce382632b2c604e
Rene <rene.exe@gmail.com>
2020-08-11 23:16:30 -0300

    Isolando teste buscar_avatar local com teste de integração

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -1,14 +1,28 @@
 from unittest.mock import Mock
 
+import pytest
+
 from libpythonpro import github_api
 
-
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url():
     resp_mock = Mock()
+    url = "https://avatars1.githubusercontent.com/u/9634767?v=4"
     resp_mock.json.return_value = {
         "login": "renexe", "id": 9634767, "node_id": "MDQ6VXNlcjk2MzQ3Njc=",
-        "avatar_url": "https://avatars1.githubusercontent.com/u/9634767?v=4"
+        "avatar_url": url
     }
-    github_api.requests.get = Mock(return_value = resp_mock)
+    get_original = github_api.requests.get
+    github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = get_original
+
+
+def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('renexe')
-    assert 'https://avatars1.githubusercontent.com/u/9634767?v=4' == url
+    assert avatar_url == url
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('renexe')
+    assert "https://avatars1.githubusercontent.com/u/9634767?v=4" == url
+
